6398606e9116f3bb37b380f1aabf67c917e741e0
Juanduque08 <juamunoz6@ielasnieves.edu.co>
2025-08-20 16:17:45 -0500

    Add files via upload

---
+++ Workspace-20250723T182602Z-1-001/Workspace/Actividad-Ej2/ejercicio1.js
@@ -0,0 +1,3 @@
+const titleclement = document.querySelector(".title")
+
+titleclement.innerHTML = "Esta pagina no es compatible con la version actual de tu navegador. Actualizar a la version mas reciente "
