fd209dc4ec9d507b48764dd2783f707037122b51
Juanduque08 <juamunoz6@ielasnieves.edu.co>
2025-10-24 16:27:40 -0500

    Add files via upload

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,14 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: 'localhost' ,
-    user: 'root' ,
-    password: '' ,
-    database:'notas'
-
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'notas'
 });
-
-connection.connect(err =>{
+connection.connect(err => {
     if (err) throw err;
-    console.log('conectado a la base de datos MySQL');
-
+    console.log('Conectado ala base de datos MYSQL');
 });
-module.exports =connection;
+
+
+module.exports = connection;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('express');
-const estudianteRoutes = require('./routes/estudiantes');
+const cors = require('cors');
+const estudiantesRoutes = require('./routes/estudiantes');
 const app = express();
 const PORT = 3000;
 app.use(cors());
 app.use(express.json());
-app.use('/api/estudiantes',estudianteRoutes);
-app.listen(PORT, () =>{
-    console.log(`'servidor http://localhost:${PORT}`);
+app.use('/api/estudiantes', estudiantesRoutes);
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
